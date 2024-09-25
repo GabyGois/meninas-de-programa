f43be1a5c946da760775444f0395877e5778a8c7
GabyGois <141441773+GabyGois@users.noreply.github.com>
2024-09-25 08:31:33 -0300

    4e

--- main.js
+++ main.js
@@ -7,3 +7,8 @@ function areaQuadrado (lado) {
     return lado*lado;
 }
 console.log (areaQuadrado(54));
+ 
+let calculoMedia = function (num3,num4,num5){
+    return (num3+num4+num5)/3;
+}
+console.log (calculoMedia(2,4,9);)
