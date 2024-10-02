436148d0529aafb550cb535c5c9aa086e6dcc7ba
GabyGois <141441773+GabyGois@users.noreply.github.com>
2024-10-02 07:39:09 -0300

    yiyio

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
-function soma (num1,num2){
-   return num1+num2;
-}
-console.log (soma(6,7)) 
+// function soma (num1,num2){
+//    return num1+num2;
+// }
+// console.log (soma(6,7)) 
 
-function areaQuadrado (lado) {
-    return lado*lado;
-}
-console.log (areaQuadrado(54));
- 
-let num3 = parseInt(prompt('digite um numero'));
-let num4 = parseInt(prompt('digite um numero'));
-let num5 = parseInt(prompt('digite um numero'));
+// function areaQuadrado (lado) {
+//     return lado*lado;
+// }
+// console.log (areaQuadrado(54));
 
-let calculoMedia = function (num3,num4,num5){
-    return (num3+num4+num5)/3;
-}
-document.write (calculoMedia (num3,num4,num5));
+// let num3 = parseInt(prompt('digite um numero'));
+// let num4 = parseInt(prompt('digite um numero'));
+// let num5 = parseInt(prompt('digite um numero'));
+
+// let calculoMedia = function (num3,num4,num5){
+//     return (num3+num4+num5)/3;
+// }
+// document.write (calculoMedia (num3,num4,num5));
+
+let estados = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO','DF'];
