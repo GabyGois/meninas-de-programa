69f3c55b5dbd1c5749011aefd29fee96811ea0f9
GabyGois <141441773+GabyGois@users.noreply.github.com>
2024-10-02 08:23:10 -0300

    shvd

--- main.js
+++ main.js
@@ -18,14 +18,25 @@
 // document.write (calculoMedia (num3,num4,num5));
 
 
-let estados = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO','DF'];
-let i = 0;
-while (i<estados.length){
-    console.log(i);
-    console.log(estados[i]);
-    i=i+1;}
+// let estados = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO','DF'];
+// let i = 0;
+// while (i<estados.length){
+//     console.log(i);
+//     console.log(estados[i]);
+//     i=i+1;}
 
-for (let i = 0; i < estados.length; i++) {
-    console.log(i);
-    console.log(estados[i]);
+// for (let i = 0; i < estados.length; i++) {
+//     console.log(i);
+//     console.log(estados[i]);
+
+// }
+
+
+let idade = 18
+if (idade>17) {
+    console.log("preso")
+    
+} else
+    { console.log( 'livre')
+    
 }
