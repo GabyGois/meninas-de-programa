084c3143fd9ccb9ac2d0d05abc9fc04fb749e35e
GabyGois <141441773+GabyGois@users.noreply.github.com>
2024-10-02 08:29:29 -0300

    n

--- main.js
+++ main.js
@@ -32,7 +32,7 @@
 // }
 
 
-let idade = 18 parseInt(prompt('andou cometendo crimes recentemente?'))
+let idade = parseInt(prompt('andou cometendo crimes recentemente?'));
 if (idade>17) {
     console.log('preso')
     
